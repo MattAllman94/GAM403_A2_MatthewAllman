a0e079de8ea6f91cae990b19b662c00cbc66179c
MattAllman94 <62230366+MattAllman94@users.noreply.github.com>
2020-03-25 19:54:02 +1100

    25 March
    Added Animation and scripts folder.
    Added Player Movement Script

---
+++ GAM403_A2_MatthewAllman/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(h, 0, v) * speed * Time.time);
+    }
+}
