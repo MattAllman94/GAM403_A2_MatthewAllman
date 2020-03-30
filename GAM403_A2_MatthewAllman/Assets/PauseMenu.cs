cb1fdfe27564ae66d27ef103585e7e61fdf0af7b
MattAllman94 <62230366+MattAllman94@users.noreply.github.com>
2020-03-30 12:57:59 +1100

    Pause Menu
    Created and coded a basic pause menu

---
+++ GAM403_A2_MatthewAllman/Assets/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        print("Game Resumed");
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        print("Game Paused");
+    }
+
+    public void LoadMenu()
+    {
+        print("Menu Loading");
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        print("Game Quit");
+        Application.Quit();
+    }
+}
